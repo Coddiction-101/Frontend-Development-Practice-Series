e09dd9800483957f47648a5637a08fc0fabc3f95
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-15 13:08:57 +0530

    Update main.js

--- day_02_LoginUI/main.js
+++ day_02_LoginUI/main.js
@@ -1,1 +1,12 @@
+const togglePassword = document.querySelector(".toggle-password");
+const passwordInput = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+
+    this.classList.toggle("ri-eye-off-line");
+});
+
 
